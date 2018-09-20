94213869d9ba77240fdc59342a523d94b0402432
Abril Anchondo Reynaga <abril79reynaga@gmail.com>
2018-09-20 18:05:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 
+function getFirstSelector(selector) {
+  return document.querySelector('selector');
+  
+}
+
+function nestedTarget() {
+  return document.getElementById('nested').querySelectorAll('div.target');
+  //document.querySelector('#nested.target');
+}
+
+function increaseRankBy(n) {
+ var  result = document.querySelectorAll("ul.ranked-list li");
+ for (var i = 0; i <result.length; i++) {
+   result[i].innerHTML = parseInt(result[i].innerHTML) + n;
+   }
+   
+}
+
+function deepestChild () {
+  var result = document.getElementById("grand-node").querySelectorAll('div');
+  var deep = [];
+  
+  
+}
