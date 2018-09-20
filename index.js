d41f542bfe59fb4514ea1b6d3626a66eae9f07a4
Abril Anchondo Reynaga <abril79reynaga@gmail.com>
2018-09-20 18:13:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 
+function getFirstSelector(selector) {
+  return document.querySelector('selector');
+  
+}
+
+function nestedTarget() {
+  return document.getElementById('nested').querySelectorAll('div.target');
+  //document.querySelector('#nested.target');
+}
+
+function increaseRankBy(n) {
+ var  result = document.querySelectorAll("ul.ranked-list li");
+ for (var i = 0; i <result.length; i++) {
+   result[i].innerHTML = parseInt(result[i].innerHTML) + n;
+   }
+   
+}
+
+function deepestChild () {
+  var result = querySelector('#grand-node div');
+  
+  
+  
+}
